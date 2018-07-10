7a2ee4de8e7b2f8e1ff57e2aae8f0d94ebae564f
EiffL <fr.eiffel@gmail.com>
2018-07-09 23:06:17 -0400

    Found muuuuuch simpler way of implementing this

--- cwl_parsl/main.py
+++ cwl_parsl/main.py
@@ -4,34 +4,21 @@ import cwltool.main
 from cwltool.loghandler import _logger
 from cwltool.context import LoadingContext, RuntimeContext
 from cwltool.flatten import flatten
-from .command_line_tool import ParslCommandLineTool
-
+from cwltool.command_line_tool import CommandLineTool
+from cwltool.errors import WorkflowException
+from schema_salad.validate import ValidationException
+import concurrent
+from concurrent.futures import ALL_COMPLETED, FIRST_COMPLETED
 import parsl
-from parsl import DataFlowKernel
-
-
-def parslMakeTool(toolpath_object, loadingContext):
-    """Factory function passed to load_tool() which creates instances of the
-    custom ParslCommandLineTool.
-    """
-
-    if isinstance(toolpath_object, dict) and toolpath_object.get("class") == "CommandLineTool":
-        return ParslCommandLineTool(toolpath_object, loadingContext)
-    return cwltool.context.default_make_tool(toolpath_object, loadingContext)
-
+from parsl import DataFlowKernel, App
 
 class ParslExecutor(cwltool.executors.JobExecutor):
     """
     Custom Parsl-based executor for a worflow
     """
-    # We can visit the workflow with this
-    def visitSteps(self, t, op):
-        if isinstance(t, cwltool.workflow.Workflow):
-            for s in t.steps:
-                op(s.tool)
-                self.visitSteps(s.embedded_tool, op)
-        else:
-            print(t)
+    def __init__(self): # type: () -> None
+        super(ParslExecutor, self).__init__()
+        self.futures = set()
 
     def run_jobs(self,
                  process,           # type: Process
@@ -54,36 +41,73 @@ class ParslExecutor(cwltool.executors.JobExecutor):
             "globals" : {"lazyErrors" : True}
         }
         dfk = DataFlowKernel(config=local_config)
-        runtimeContext.dfk = dfk
-        print(runtimeContext.dfk)
-        print("--------------- Beginning visit")
-        self.visitSteps(process, lambda x: print(x['id']))
-        print("--------------- Done visit")
+
+        @App('python', dfk)
+        def parsl_runner(job, rc, inputs=[]):
+            """
+            The inputs are dummy futures provided by the parent steps of the
+            workflow, if there are some
+            """
+            return job.run(rc)
+
+        process_run_id = None  # type: Optional[str]
+        reference_locations = {}  # type: Dict[Text,Text]
+
+        # define provenance profile for single commandline tool
+        if not isinstance(process, cwltool.workflow.Workflow) \
+                and runtimeContext.research_obj is not None:
+            orcid = runtimeContext.orcid
+            full_name = runtimeContext.cwl_full_name
+            process.provenance_object = CreateProvProfile(
+                runtimeContext.research_obj, orcid, full_name)
+            process.parent_wf = process.provenance_object
         jobiter = process.job(job_order_object, self.output_callback, runtimeContext)
-        for j in jobiter:
-            if j:
-                print(j)
-                res = j.run(runtimeContext)
-                if res:
-                    print(res.result())
-            else:
-                logger.error("Workflow cannot make any more progress.")
-                break
 
-    def execute(self,
-                process,           # type: Process
-                job_order_object,  # type: Dict[Text, Any]
-                runtimeContext,    # type: RuntimeContext
-                logger=_logger,
-               ):  # type: (...) -> Tuple[Optional[Dict[Text, Any]], Text]
-        """ Execute the process. """
+        try:
+            for job in jobiter:
+                if job:
+                    if runtimeContext.builder is not None:
+                        job.builder = runtimeContext.builder
+                    if job.outdir:
+                        self.output_dirs.add(job.outdir)
+                    if runtimeContext.research_obj is not None:
+                        if not isinstance(process, Workflow):
+                            runtimeContext.prov_obj = process.provenance_object
+                        else:
+                            runtimeContext.prov_obj = job.prov_obj
+                        assert runtimeContext.prov_obj
+                        process_run_id, reference_locations = \
+                                runtimeContext.prov_obj.evaluate(
+                                        process, job, job_order_object,
+                                        runtimeContext.make_fs_access,
+                                        runtimeContext)
+                        runtimeContext = runtimeContext.copy()
+                        runtimeContext.process_run_id = process_run_id
+                        runtimeContext.reference_locations = reference_locations
 
-        self.run_jobs(process, job_order_object, logger, runtimeContext)
+                    # Run the job within a Parsl App
+                    res = parsl_runner(job, runtimeContext)
 
-        return  (None, "permanentFail")
+                    # Adds the future to the set of futures
+                    self.futures.add(res.parent)
+                else:
+                    print(self.futures)
+                    if self.futures:
+                        done, notdone = concurrent.futures.wait(self.futures, return_when=FIRST_COMPLETED)
+                        self.futures = notdone
+                    else:
+                        logger.error("Workflow cannot make any more progress.")
+                        break
+
+            # At the end, make sure all the steps have been processed
+            if self.futures:
+                concurrent.futures.wait(self.futures, return_when=ALL_COMPLETED)
+        except (ValidationException, WorkflowException):
+            raise
+        except Exception as e:
+            logger.exception("Got workflow error")
+            raise WorkflowException(Text(e))
 
 if __name__ == "__main__":
-
     sys.exit(cwltool.main.main(sys.argv[1:],
-             executor=ParslExecutor(),
-             loadingContext=LoadingContext(kwargs={'construct_tool_object':parslMakeTool})))
+             executor=ParslExecutor()))
